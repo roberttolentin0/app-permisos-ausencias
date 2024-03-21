14273e26fec5ace7d1d57233850888bd171e6bcc
Robert Carlos <robert.tolentino@terranovatrading.com.pe>
2024-03-21 10:37:09 -0500

    Add load dotenv

--- src/services/microsoft_teams_service.py
+++ src/services/microsoft_teams_service.py
@@ -1,8 +1,12 @@
+import os
 import threading
 import time
 import requests
 import psycopg2
 import json
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Variable global para controlar el cron
 cron_activo = False
@@ -12,10 +16,11 @@ class DatabaseManager:
     def __init__(self):
         # Conectarse a la base de datos
         self.conn = psycopg2.connect(
-            dbname="db_permisos",
-            user="postgres",
-            password="1234",
-            host="localhost"
+            dbname=os.getenv('DB_DATABASE'),
+            user=os.getenv('DB_USER'),
+            password=os.getenv('DB_PASSWORD'),
+            host=os.getenv('DB_HOST'),
+            port=os.getenv('DB_PORT'),
         )
 
     def connection(self):
