7be13b1c556172b82b51a1c615f09461c4dea0de
Robert Carlos <robert.tolentino@terranovatrading.com.pe>
2024-03-22 14:50:18 -0500

    Add cors

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
+from flask import Flask
+from flask_cors import CORS
 from config import config
 from src import init_app
 
 configuration = config['development']
 app = init_app(configuration)
+CORS(app)  # Agregar esta línea para habilitar CORS en tu aplicación
 
 if __name__ == '__main__':
-    # app = init_app()
     app.run()
